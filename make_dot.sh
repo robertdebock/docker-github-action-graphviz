9f83cda1733496358ea5f0df35c671f5c9e445d9
Robert de Bock <robert@meinit.nl>
2021-01-13 12:03:07 +0100

    Fix issue that created mutliple graphs instead of multiple nodes.

--- make_dot.sh
+++ make_dot.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
 this_role=$(cat meta/main.yml | shyaml get-value galaxy_info.role_name)
-cat requirements.yml | shyaml get-value roles | while read dash name role rest ; do cat << EOF
+output_file=requirements.dot
+
+cat << EOF > "${output_file}"
 digraph PhiloDilemma {
   label = "Robert de Bock" ;
   overlap=false
   {
     "${this_role}" [fillcolor=yellow style=filled penwidth=3]
-    "${role}" [fillcolor=green style=filled]
-  }
-  "${this_role}" -> "${role}"
-}
 EOF
-done > requirements.dot
+
+cat requirements.yml | shyaml get-value roles | while read dash name role rest ; do
+    echo "    \"${role}\" [fillcolor=green style=filled]" >> "${output_file}"
+done
+
+echo "  }" >> "${output_file}"
+
+cat requirements.yml | shyaml get-value roles | while read dash name role rest ; do
+  echo "  \"${this_role}\" -> \"${role}\"" >> "${output_file}"
+done
+
+echo "}" >> "${output_file}"
